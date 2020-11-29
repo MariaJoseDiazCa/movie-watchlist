6f1edec13734d480bf62f85dcdad12427df66b50
MariaJoseDiazCa <mariajose.diazca@gmail.com>
2020-11-29 19:12:27 -0300

    UPDATE: MovieControls, Watched and Watchlist components

--- src/components/MovieControls.js
+++ src/components/MovieControls.js
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
-export const MovieControls = ({ movie, type }) => {
+export const MovieControls = ({ type, movie }) => {
+  const { removeMovieFromWatchlist, addMovieToWatched } = useContext(
+    GlobalContext
+  );
   return (
     <div className="inner-card-controls">
       {type === "watchlist" && (
         <>
-          <button className="ctrl-btn">
+          <button className="ctrl-btn" onClick={() => addMovieToWatched(movie)}>
             <i className="fa-fw far fa-eye"></i>
           </button>
 
-          <button className="ctrl-btn">
-            <i className="fa-fw far fa-times"></i>
+          <button
+            className="ctrl-btn"
+            onClick={() => removeMovieFromWatchlist(movie.id)}
+          >
+            <i className="fa-fw fa fa-times"></i>
+          </button>
+        </>
+      )}
+
+      {type === "watched" && (
+        <>
+          <button className="ctrl-btn" onClick={() => addMovieToWatched(movie)}>
+            <i className="fa-fw far fa-eye-slash"></i>
+          </button>
+
+          <button
+            className="ctrl-btn"
+            onClick={() => removeMovieFromWatchlist(movie.id)}
+          >
+            <i className="fa-fw fa fa-times"></i>
           </button>
         </>
       )}
